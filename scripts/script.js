291f1b3aa9709e1aa18d92dbbaf568e384ab5440
am1macdonald <am1macdonald@gmail.com>
2021-05-03 05:13:08 -0400

    -gameboard is displaying -function to populate gameboard with divs

--- scripts/script.js
+++ scripts/script.js
@@ -1,18 +1,48 @@
-const GameBoard = () => {
-
-};
-
-
-const Player = (name, score) => {
-
-    this.player
-
-};
-
-const Game = () => {
+const Game = (() => {
 
     const round = () => {
 
-    }
+    };
+
+    const Game = () => {
+
+        const round = () => {
+    
+        };
+
+        const playEx = () => {
+
+        };
+
+        const playOh = () => {
+
+        };
+    
+    };
+
+    const displayBoard = (() => {
+        let gameboard = document.getElementById("gameboard");
+        let gameCell = document.createElement('div');
+        gameCell.classList.add("game-cell");
+        let symbolArr = [10060, 9711];
+        for (let i = 1; i < 10; i++){
+            let cellClone = gameCell.cloneNode();
+            let symbol = String.fromCharCode(symbolArr[Math.round(Math.random() * 1)]);
+            cellClone.setAttribute("id", `cell-${i}`);
+            cellClone.innerText = symbol;
+            cellClone.classList.add("flex-center")
+            gameboard.appendChild(cellClone);         
+        };
+
+    })();
+
+})();
+
+
+const Player = (name, exOrOh) => {
+
+    this.playerName = name;
+
 
 };
+
