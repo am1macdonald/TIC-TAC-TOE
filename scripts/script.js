e03ca2066b92cea500f8905d6aa3e5919e938ff4
am1macdonald <am1macdonald@gmail.com>
2021-05-04 02:53:03 -0400

    game prints random, placeholder symbol when cell is clicked

--- scripts/script.js
+++ scripts/script.js
@@ -1,48 +1,49 @@
 const Game = (() => {
 
-    const round = () => {
+    const symbolArr = [String.fromCharCode(10060), String.fromCharCode(9711)];
 
+    const Player = (name, pieceSelection) => {
+        this.playerName = name;
+        this.symbol = pieceSelection;        
+        const sayPiece = () => alert(`you play ${symbol}`);
+        const makeSelection = () => symbol;
+        const sayName = () => console.log(playerName);
+        return { sayName };
     };
+    const Gameboard = () => {
+        this.board = ['','','','','','','','',''];
+        return { board };
+    };    
 
-    const Game = () => {
-
-        const round = () => {
-    
-        };
-
-        const playEx = () => {
-
-        };
-
-        const playOh = () => {
-
-        };
-    
-    };
+    const NewRound = () => {
+        let playerOne = Player(prompt("Whats ur name?"), prompt(`${symbolArr[0]} or ${symbolArr[1]}`));
+        let playerTwo = Player(prompt("Whats ur name?"), prompt(`${symbolArr[0]} or ${symbolArr[1]}`));
+        playerOne.sayName();
+        playerTwo.sayName();
+    };  
 
     const displayBoard = (() => {
-        let gameboard = document.getElementById("gameboard");
+        const currentGame = Gameboard();
+        let board = document.getElementById("gameboard");
         let gameCell = document.createElement('div');
-        gameCell.classList.add("game-cell");
-        let symbolArr = [10060, 9711];
-        for (let i = 1; i < 10; i++){
+        gameCell.classList.add('game-cell');
+        for (let i = 0; i < 9; i++){
             let cellClone = gameCell.cloneNode();
-            let symbol = String.fromCharCode(symbolArr[Math.round(Math.random() * 1)]);
-            cellClone.setAttribute("id", `cell-${i}`);
-            cellClone.innerText = symbol;
-            cellClone.classList.add("flex-center")
-            gameboard.appendChild(cellClone);         
-        };
 
+            let symbol = symbolArr[Math.round(Math.random() * 1)];
+
+            cellClone.addEventListener("click", function() {
+                currentGame.board[i] = symbol
+                console.log(currentGame.board);
+                cellClone.innerText = symbol;
+            });
+
+            cellClone.setAttribute("id", `cell-${i}`);            
+            cellClone.classList.add("flex-center");
+            board.appendChild(cellClone);         
+        };
     })();
+    
+    return { NewRound };
 
 })();
-
-
-const Player = (name, exOrOh) => {
-
-    this.playerName = name;
-
-
-};
-
