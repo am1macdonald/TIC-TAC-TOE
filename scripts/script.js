209aeba2e9a55e7073e723f022fc469cf4c3815c
am1macdonald <am1macdonald@gmail.com>
2021-05-05 02:33:26 -0400

    added eventHandler for grid clicks

--- scripts/script.js
+++ scripts/script.js
@@ -1,11 +1,11 @@
-const Game = (() => {
+(function() {
 
     const symbolArr = [String.fromCharCode(10060), String.fromCharCode(9711)];
 
     const Player = (name, pieceSelection) => {
         this.playerName = name;
         this.symbol = pieceSelection;        
-        const sayPiece = () => alert(`you play ${symbol}`);
+        const sayPiece = () => console.log(`you play ${symbol}`);
         const makeSelection = () => symbol;
         const sayName = () => console.log("Player: " + playerName);
         return { sayName, sayPiece, makeSelection, name };
@@ -21,47 +21,53 @@ const Game = (() => {
             alert("nice try pal...");
             location.reload();
         };
-        const playerOne = Player(prompt("Whats player 1's name?"), symbolArr[0]);
+
+        function nextTurn(element) {
+
+            console.log(element.id);
+        };
+
+        const playerOne = Player("A", symbolArr[0]);
         if (!playerOne.name) {
             errorMessage();
         };
         playerOne.sayName();
         playerOne.sayPiece();
-        console.log(playerOne);
 
-        const playerTwo = Player(prompt("Whats player 2's name?"), symbolArr[1]);
+        const playerTwo = Player("B", symbolArr[1]);
         if (!playerTwo.name) {
             errorMessage();
         };      
         playerTwo.sayName();
         playerTwo.sayPiece();
-        console.log(playerTwo);
 
-        return { playerOne, playerTwo };
-    })();  
-
-    const displayBoard = (() => {
-        
-        let board = document.getElementById("gameboard");
-        let gameCell = document.createElement('div');
-        gameCell.classList.add('game-cell');
-        for (let i = 0; i < 9; i++){
-            let cellClone = gameCell.cloneNode();
-
-            let symbol = symbolArr[Math.round(Math.random() * 1)];
-
-            cellClone.addEventListener("click", function() {
-                currentGame.board[i] = symbol;
-                console.log(currentGame.board);
-                cellClone.innerText = symbol;
-            });
-
-            cellClone.setAttribute("id", `cell-${i}`);            
-            cellClone.classList.add("flex-center");
-            board.appendChild(cellClone);         
+        const checkGameOver = () => {
+            switch (gameboard.board){
+                case (board[0] === board[1] && board[1] === board[2]):
+                    console.log(`${board[0]} wins the game!`);
+                    break;
+                default:
+                    nextTurn();
+            };
         };
-        return {
-            errorMessage
-        };
+
+        return { nextTurn };
+    })(); 
+
+    const cacheDom = (() => {
+        gridArray = Array.from(document.getElementsByClassName("game-cell"));
     })();
+
+    const bindEvents = (() => {
+        gridArray.forEach(element => {
+            element.addEventListener('click', () => { gamePlay.nextTurn(element) });
+        });
+    })();
+
+    const displayController = (() => {
+
+        const render = () => {};
+        
+    })();
+    return { cacheDom }
 })();
